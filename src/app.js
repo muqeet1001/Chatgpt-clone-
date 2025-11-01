be252fe238758adf8dcdbe36b476ab6c577c0bef
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-01 22:13:46 +0530

    implement user authentication with signup and login routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./db/db');
-
+const authRoutes = require('./routes/auth.route');
+const bcrypt = require('bcrypt');
 const app = express();
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,52 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+// User Signup Controller
+async function signup(req, res) {
+  const {
+    fullName: { firstName, lastName },
+    email,
+    password,
+  } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      fullName: { firstName, lastName },
+      email: email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully", newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+async function login(req, res) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, { httpOnly: true });
+    res.status(200).json({ message: "Login successful", user });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = { signup, login };
